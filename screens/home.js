c84b8156cd2730c2c7aed6fc75a3e028d8992699
Riccardo Kiss <36801806+RiccardoKiss@users.noreply.github.com>
2022-04-17 01:42:31 +0200

    Upravenie vzhladu tasku a pridanie buttonu na testovaci POST

--- screens/home.js
+++ screens/home.js
@@ -9,6 +9,7 @@ import {
     Pressable,
     Text,
     View,
+    KeyboardAvoidingView,
   } from 'react-native';
 
 import Task from '../components/task.js'
@@ -20,14 +21,13 @@ const Home = () => {
 
     const [isLoaded, setIsLoaded] = useState(false);
     const [reload, setReload] = useState(false);
-    const [pageJson, setPageJson] = useState({});
+    const [taskItems, setTaskItems] = useState([]);
     let page = {};
 
     const getTasks = async url => {
         await fetch(url)
         .then(function(response) {
             if (!response.ok) {
-                console.log("notok")
                 throw Error(response.statusText);
             }
             return response;
@@ -35,32 +35,88 @@ const Home = () => {
         .then(response => response.json())   
         .then(data => {
             console.log(data);
-            setPageJson(data);
+            setTaskItems([...taskItems, data]);
             setReload(false);
             setIsLoaded(true);
         })
     }
     
+    const postTask = async url => {
+        const requestOptions = {
+            method: 'POST',
+            headers: { 'Content-Type': 'application/json' },
+            body: JSON.stringify({
+                userid: 1,
+                targetid: 2,
+                name: "Odpadky",
+                objective: "Chod vyhodit smeti!",
+                completion: 0
+            })
+        };
+        await fetch(url, requestOptions)
+        .then(function(response) {
+            if (!response.ok) {
+                throw Error(response.statusText);
+            }
+            return response;
+        }).catch(error => {setReload(true); console.log(error)})
+        .then(response => response.json())   
+        .then(data => {
+            console.log(data);
+            setTaskItems([...taskItems, data]);
+            //setReload(false);
+            setIsLoaded(true);
+        })
+    }
+
     useEffect(() => {
         if(!isLoaded){
-            getTasks('http://' + global.ip + '0/bckend/tasks/view');
+            getTasks('http://' + global.ip + '/bckend/tasks/view');
         }
-        
-        console.log("asdasd");
       }, []);
 
-    return(
-        <SafeAreaView style={styles.sectionContainer}>
-            <ScrollView>
-                {isLoaded ? pageJson.items.map((item) => <Task name={item.name} objective={item.objective} completion={item.completion}/>)  : <Text>Loading...</Text>}
-                {reload ? 
-                    <Pressable style={styles.inputButton} android_ripple={{color:'grey'}} onPress={() => getTasks('http://' + global.ip + '/bckend/tasks/view')}>
-                        <Text>
-                            Reload
-                        </Text>
-                    </Pressable> : <Text></Text>}
-            </ScrollView>
-        </SafeAreaView>
+    return (
+      <SafeAreaView style={styles.sectionContainer}>
+        <ScrollView>
+          {
+            isLoaded 
+              ? taskItems.map((item, index) => {
+                  return (
+                    <View key={index}>
+                      <Task userID={item.userid} targetID={item.targetid} name={item.name} objective={item.objective} completion={item.completion}/>
+                    </View>
+                  )
+                }) 
+              : <Text>Loading...</Text>
+          }
+          { 
+            reload 
+              ? <Pressable 
+                  style={styles.inputButton} 
+                  android_ripple={{color:'grey'}} 
+                  onPress={() => getTasks('http://' + global.ip + '/bckend/tasks/view')}
+                >
+                  <Text> Reload </Text>
+                </Pressable> 
+              : <Text></Text>
+          }
+        </ScrollView>
+        <View style={styles.addTaskView}>
+          <Pressable
+            style={styles.addTaskButton} 
+            android_ripple={{color:'grey', borderless: true}} 
+            onPress={() => postTask('http://' + global.ip + '/bckend/tasks/create')}
+          >
+            <Text style={styles.plusText}>+</Text>
+          </Pressable>
+        </View>
+        
+        {/* 
+        <KeyboardAvoidingView 
+          behavior={Platform.OS === "ios" ? "padding" : "height"}
+          style={styles.addTask}
+        ></KeyboardAvoidingView>*/}
+      </SafeAreaView>
     );
 }
 
@@ -92,6 +148,27 @@ const styles = StyleSheet.create({
     },
     highlight: {
         fontWeight: '700',
+    },
+    addTaskView: {
+        overflow: 'hidden',
+        borderRadius: 60,
+        justifyContent: 'center',
+        alignSelf: 'center',
+        margin: 10,
+    },
+    addTaskButton: {
+        width: 60,
+        height: 60,
+        backgroundColor: '#FFF',
+        justifyContent: 'center',
+        alignItems: 'center',
+        borderRadius: 60,
+        borderColor: '#C0C0C0',
+        borderWidth: 1,
+        padding: 10,
+    },
+    plusText: {
+        fontSize: 25,
     },
 });
 
